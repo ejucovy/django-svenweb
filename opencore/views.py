ef83e7bca7ca76544981da2b64b017fec6f95fbb
Ethan Jucovy <ejucovy@gmail.com>
2011-06-23 14:31:19 -0400

    stub view

--- opencore/views.py
+++ opencore/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.http import HttpResponse
+from djangohelpers.lib import rendered_with, allow_http
+
+@allow_http("GET")
+def home(request):
+    return HttpResponse("welcome to the site, %s" % request.user)
