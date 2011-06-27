bfe3b3d336fb4f3b27da5eb811200d74f2a7d5dc
Ethan Jucovy <ejucovy@gmail.com>
2011-06-27 13:10:58 -0400

    add index_url templatefilter

--- sites/templatetags/repo.py
+++ sites/templatetags/repo.py
@@ -16,6 +16,10 @@ def edit_url(wiki, path):
 register.filter("page_edit_url", edit_url)
 
 @register.filter
+def index_url(wiki, path):
+    return wiki.directory_index_url(path)
+
+@register.filter
 def page_create_url(wiki, path):
     return wiki.page_create_url(path)
 
