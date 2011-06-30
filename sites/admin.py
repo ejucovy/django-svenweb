783c3b99c4243ff144356747053a2bb1bf79426a
Ethan Jucovy <ejucovy@gmail.com>
2011-06-30 07:44:46 -0400

    register admin model

--- sites/admin.py
+++ sites/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from svenweb.sites.models import Wiki, UserProfile, UserWikiLocalRoles
+from svenweb.sites.models import (Wiki, UserProfile, 
+                                  UserWikiLocalRoles,
+                                  WikiRolePermissions)
 
 admin.site.register(Wiki)
 admin.site.register(UserProfile)
 admin.site.register(UserWikiLocalRoles)
-
+admin.site.register(WikiRolePermissions)
